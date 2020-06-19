29867e4afd034788b3290866e862761efd111bab
Jonathan Ray <jonathanray17@gmail.com>
2020-06-18 22:06:20 -0400

    debugging forecastrouter

--- routes/forecast.js
+++ routes/forecast.js
@@ -3,6 +3,14 @@ const Router = express.Router;
 const forecastRouter = Router();
 const { addForecast, sendForecast } = require('../controllers/forecast');
 
-forecastRouter.post('/', addForecast, sendForecast);
+forecastRouter.post(
+	'/',
+	(req, res, next) => {
+		console.log('made it to the forecastRouter');
+		next();
+	},
+	addForecast,
+	sendForecast,
+);
 
 module.exports = forecastRouter;
