d06bb04f98c25bf458acbc35190656fb136bd26f
Jonathan Ray <jonathanray17@gmail.com>
2020-06-30 09:07:15 -0400

    debugging subscriber validation

--- controllers/subscriber.js
+++ controllers/subscriber.js
@@ -42,7 +42,7 @@ async function validateSubscriber(req, res) {
 		decodeURIComponent(email),
 	);
 	if (!validSubscriber.id) res.sendStatus(503);
-	else res.redirect('http://hurricanecast.com');
+	else res.redirect('https://hurricanecast.com');
 }
 
 async function removeSubscriber(req, res) {
